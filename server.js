8f0a6efdfef392f3ead0f6733ab306d48f658c0c
Matt Sweeney <msweeney777@gmail.com>
2018-12-22 15:38:55 -0500

    Added a description to St. Brendan of Clonfert, made some changes to server.js

--- server.js
+++ server.js
@@ -18,25 +18,25 @@ app.get('/', (req, res) => {
 
 app.get('/Ciaran_of_Saigir', (req, res) => {
   res.render('St. Ciaran.hbs', {
-    pageTitle: 'St. Ciarán of Saigir'
+    pageTitle: 'St. Ciarán of Saigir (5th century - 530)'
   });
 });
 
 app.get('/Ciaran_of_Clonmacnoise', (req, res) => {
   res.render('St. Ciarán of Clonmacnoise.hbs', {
-    pageTitle: 'St. Ciarán of Clonmacnoise'
+    pageTitle: 'St. Ciarán of Clonmacnoise (516 - 549)'
   });
 });
 
 app.get('/Brendan_of_Birr', (req, res) => {
   res.render('St. Brendan of Birr.hbs', {
-    pageTitle: 'St. Brendan of Birr'
+    pageTitle: 'St. Brendan of Birr (d. 572)'
   });
 });
 
 app.get('/Brendan_of_Clonfert', (req, res) => {
   res.render('St. Brendan of Clonfert.hbs', {
-    pageTitle: 'St. Brendan of Clonfert'
+    pageTitle: 'St. Brendan of Clonfert (484 - 577)'
   });
 });
 
